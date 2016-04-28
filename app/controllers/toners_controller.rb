305f864d6d527f421315b45f53fec241da822eb2
Donapieppo <donapieppo@yahoo.it>
2016-04-28 13:50:41 +0200

    fix bug

--- app/controllers/toners_controller.rb
+++ app/controllers/toners_controller.rb
@@ -1,15 +1,14 @@
 class TonersController < ApplicationController
-  before_action :set_organization_and_check_permission, except: [:edit, :update, :destroy]
   before_action :set_toner_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
-    @toner = @organization.toners.new
+    @toner = @current_organization.toners.new
   end
 
   def create
-    @toner = @organization.toners.new(toner_params)
+    @toner = @current_organization.toners.new(toner_params)
     if @toner.save
-      redirect_to organization_path(@organization), notice: 'il toner è stato aggiunto come disponibile.'
+      redirect_to organization_path(@current_organization), notice: 'il toner è stato aggiunto come disponibile.'
     else
       render :new
     end
@@ -27,12 +26,12 @@ class TonersController < ApplicationController
     end
   end
 
+  def destroy
+    @toner.delete 
+    redirect_to organization_path(@current_organization), notice: 'il toner è stato eliminato da quelli disponibili.'
+  end
 
   private
-
-  def set_organization_and_check_permission
-    @organization = Organization.find(params[:organization_id])
-  end
 
   def toner_params
     params[:toner].permit(:number, :toner_model_id)
