baf1871ba54ec83a8283e9c9d8fa5a703e2cf712
Donapieppo <donapieppo@yahoo.it>
2016-04-29 17:00:30 +0200

    comments

--- doc/got_example.rb
+++ doc/got_example.rb
@@ -6,9 +6,9 @@ CESIA_UPN = [ 'name.surname@unibo.it', 'name2.surname2@unibo.it' ]
 module Got
   class Application < Rails::Application
     config.dm_common = {
-      login_method: :log_if_email,
-      message_footer: %Q{Messaggio inviato da 'Game of Toner'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare got-support@unibo.it},
-      impersonate_admins:  ['name.surname@unibo.it'],
+      login_method: :log_if_email, # can use application only if present in database
+      message_footer: %Q{Messaggio inviato da 'Game of Toner'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare got-support@unibo.it},       # mailer 
+      impersonate_admins:  ['name.surname@unibo.it'], # who can impersonate. See https://github.com/ankane/pretender
       interceptor_mails:   ['name@example.com'],
       main_impersonations: [] }
   end
