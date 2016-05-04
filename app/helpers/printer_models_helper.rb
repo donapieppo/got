3a50b43f79cc36aa9119477b5a9e9524192dbff4
Donapieppo <donapieppo@yahoo.it>
2016-05-04 10:47:20 +0200

    show printer_model helper

---
+++ app/helpers/printer_models_helper.rb
@@ -0,0 +1,10 @@
+module PrinterModelsHelper
+  def show_printer_model_with_actions(printer_model)
+    res = html_escape printer_model.name 
+    res += content_tag(:span, class: :actions) do 
+      link_to_edit(edit_printer_model_path(printer_model)) +
+      link_to(icon('plus'), new_printer_model_printer_path(printer_model), title: 'add printer')
+    end
+  end
+end 
+
