05fdb7fe7f1ddfa169750c3e06bc03bdc9238d74
Donapieppo <donapieppo@yahoo.it>
2016-05-30 17:21:03 +0200

    subscribe to create new organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   include DmCommon::Controllers::Helpers
@@ -10,8 +8,12 @@ class ApplicationController < ActionController::Base
   before_filter :log_current_user, :force_sso_user, :retrive_authlevel
 
   def retrive_authlevel
-    @current_organization = current_user.organizations.first or raise DmCommon::NoAccess
-    @available_organizations = current_user.organizations
-  end
+    @available_organizations = current_user.organizations.all
 
+    if @available_organizations.empty?
+      redirect_to new_subscription_path and return
+    end
+
+    @current_organization = @available_organizations.first
+  end
 end
