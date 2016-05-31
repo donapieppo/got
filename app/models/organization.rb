76c9959383571244e0f1510fabdf4915cb17c94b
Donapieppo <donapieppo@yahoo.it>
2016-05-31 09:49:44 +0200

    refactor printer_models and toner for organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,7 +5,9 @@ class Organization < ActiveRecord::Base
 
   validates :name, uniqueness: {}
 
-  attr_accessor :_printer_model_ids, :_toner_models
+  # cache
+  attr_accessor :_printer_model_ids, 
+                :_usable_toner_models
 
   def to_s
     self.name + " (" + self.description + ")"
@@ -15,25 +17,24 @@ class Organization < ActiveRecord::Base
     "#{self.name} - #{self.description[0..70]}"
   end
 
-  def printer_model_ids
-    @_printer_model_ids ||= self.printers.map(&:printer_model_id).uniq
-  end
-
-  def toner_models
-    @_toner_models ||= TonerModel.includes(:printer_models).where('printer_models_toner_models.printer_model_id': printer_model_ids)
-  end
-
-  # toners (in tutte organizations) for my printers
-  def available_toners
-    Toner.where(toner_model: toner_models.map(&:id)).where(gift: true)
-  end
-
   def admins_string
     self.admins.map {|admin| admin.to_s}.join(', ')
   end
 
-  def interested?(toner)
-    @printer_model_ids ||= self.printers.map(&:printer_model_id)
-    (toner.toner_model.printer_model_ids.to_a & @printer_model_ids).size > 0 
+  def printer_model_ids
+    @_printer_model_ids ||= self.printers.select(:printer_model_id).map(&:printer_model_id).uniq
+  end
+
+  def usable_toner_models
+    @_usable_toner_models ||= TonerModel.includes(:printer_models).where('printer_models_toner_models.printer_model_id': printer_model_ids)
+  end
+
+  # toners (in tutte organizations) for my printers
+  def available_toners
+    Toner.where(toner_model: usable_toner_models.map(&:id)).where(gift: true)
+  end
+
+  def interested_in?(toner)
+    usable_toner_models.map(&:id).include?(toner.toner_model_id)
   end
 end
