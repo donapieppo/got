a54cc65163113655a0ebca47e3ec7ce98e01fb12
Pietro <donapieppo@yahoo.it>
2016-06-01 11:12:10 +0200

    avevo scordato uniqueness in name

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,6 +2,7 @@ class Vendor < ActiveRecord::Base
   has_many :printer_models
   has_many :toner_models
 
+  validates :name, uniqueness: {}
   scope :with_printer_models, -> { where("id IN (SELECT DISTINCT(vendor_id) FROM printer_models)") }
 
   def to_s
