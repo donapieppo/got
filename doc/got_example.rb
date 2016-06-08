ddaaa40a0e98cdc2b90efa4b0735d11340f1a9a3
Pietro <donapieppo@yahoo.it>
2016-06-08 13:15:15 +0200

    configuratione example

--- doc/got_example.rb
+++ doc/got_example.rb
@@ -1,18 +1,23 @@
-# superusers of the application got
+require 'omniauth-google-oauth2'
+
+# CESIA are superusers of the application who
 # can manage/create organizations and add the first admin
 # to every organization
 CESIA_UPN = [ 'name.surname@unibo.it', 'name2.surname2@unibo.it' ]
+
+DmUniboCommon::LOGO_IMAGE = 'dm_unibo_common/cc.png'
 
 module Got
   class Application < Rails::Application
     config.domain_name = 'unibo.it'
     config.dm_unibo_common = {
-      omniauth_provider: :google_oauth2,
-      login_method: :log_if_email, # can use application only if present in database
-      message_footer: %Q{Messaggio inviato da 'Game of Toners'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare got-support@example.com},   # mailer 
+      omniauth_provider:   :google_oauth2,
+      login_method:        :log_if_email,             # can use application only if present in database
       impersonate_admins:  ['name.surname@unibo.it'], # who can impersonate. See https://github.com/ankane/pretender
-      interceptor_mails:   ['name@example.com'],
-      main_impersonations: [] }
+      interceptor_mails:   ['name@example.com'],      # who gets emails in development
+      main_impersonations: [],                        # suggested impersonation users
+      message_footer:      %Q{Message sent from 'Game of Toners'.\nDo not reply}, # footer in all mail
+      support_mail:        'example-supportweb@example.com' }
   end
 end
 
