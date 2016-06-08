da67ba8bed120c5ea72da2084cd0b93195ba5fb6
Pietro <donapieppo@yahoo.it>
2016-06-08 11:13:25 +0200

    example config

--- doc/got_example.rb
+++ doc/got_example.rb
@@ -6,10 +6,10 @@ CESIA_UPN = [ 'name.surname@unibo.it', 'name2.surname2@unibo.it' ]
 module Got
   class Application < Rails::Application
     config.domain_name = 'unibo.it'
-    config.dm_common = {
+    config.dm_unibo_common = {
       omniauth_provider: :google_oauth2,
       login_method: :log_if_email, # can use application only if present in database
-      message_footer: %Q{Messaggio inviato da 'Game of Toners'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare got-support@unibo.it},       # mailer 
+      message_footer: %Q{Messaggio inviato da 'Game of Toners'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare got-support@example.com},   # mailer 
       impersonate_admins:  ['name.surname@unibo.it'], # who can impersonate. See https://github.com/ankane/pretender
       interceptor_mails:   ['name@example.com'],
       main_impersonations: [] }
