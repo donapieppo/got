aff66ed39efef38c5836006cccb80e537759143e
Pietro <donapieppo@yahoo.it>
2016-06-20 11:24:01 +0200

    http://api.rubyonrails.org/v4.2/classes/ActiveSupport/HashWithIndifferentAccess.html#method-i-update

--- doc/got_example.rb
+++ doc/got_example.rb
@@ -5,12 +5,10 @@ require 'omniauth-google-oauth2'
 # to every organization
 CESIA_UPN = [ 'name.surname@unibo.it', 'name2.surname2@unibo.it' ]
 
-DmUniboCommon::LOGO_IMAGE = 'dm_unibo_common/cc.png'
-
 module Got
   class Application < Rails::Application
     config.domain_name = 'example.com'
-    config.dm_unibo_common.merge {
+    config.dm_unibo_common.update(
       omniauth_provider:   :google_oauth2,
       login_method:        :log_if_email,                # can use application only if present in database
       impersonate_admins:  ['name.surname@example.com'], # who can impersonate. See https://github.com/ankane/pretender
@@ -18,7 +16,7 @@ module Got
       main_impersonations: [],                           # suggested impersonation users
       message_footer:      %Q{Message sent from 'Game of Toners'.\nDo not reply}, # footer in all mail
       support_mail:        'example-supportweb@example.com' 
-    }
+    )
   end
 end
 
