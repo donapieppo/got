3bae938d9c9569a231a92dccc55a02905c56438e
Donapieppo <donapieppo@yahoo.it>
2019-05-21 13:15:58 +0200

    configs

--- doc/got_example_docker.rb
+++ doc/got_example_docker.rb
@@ -1,3 +1,5 @@
+require 'omniauth-google-oauth2'
+
 # CESIA are superusers of the application who
 # can manage/create organizations and add the first admin
 # to every organization
@@ -6,6 +8,11 @@ CESIA_UPN = [ 'name.surname@unibo.it', 'name2.surname2@unibo.it' ]
 module Got
   class Application < Rails::Application
     config.domain_name = 'example.com'
+
+    config.header_title    = 'Game of Toners'
+    config.header_subtitle = 'Gestione toner dismessi'
+    config.header_icon     = 'tint'
+
     config.dm_unibo_common.update(
       login_method:        :log_if_email,                # can use application only if present in database
       impersonate_admins:  ['name.surname@example.com'], # who can impersonate. See https://github.com/ankane/pretender
@@ -16,5 +23,4 @@ module Got
     )
   end
 end
-
 
