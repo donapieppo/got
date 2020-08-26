bfba5247bb5866d0bc65e49a2c7abff7ae0b9436
Donapieppo <donapieppo@yahoo.it>
2020-08-26 16:22:41 +0200

    new dm_unibo_common

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'sqlite3'
-
 #gem 'dm_unibo_common', path: '/home/rails/gems/dm_unibo_common/'
-gem 'dm_unibo_common',      git: 'https://github.com/donapieppo/dm_unibo_common.git',      ref: 'master', branch: 'master'
-gem 'dm_unibo_user_search', git: 'https://github.com/donapieppo/dm_unibo_user_search.git', ref: 'master', branch: 'master'
+gem 'dm_unibo_common',      git: 'https://github.com/donapieppo/dm_unibo_common.git'
+gem 'dm_unibo_user_search', git: 'https://github.com/donapieppo/dm_unibo_user_search.git'
+
+gem 'webpacker', '~> 4.0'
 
 gem "prawn"
 gem "prawn-table"
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,31 @@
 Rails.application.routes.draw do
-  resources :organizations do
-    resources :admins
+  mount DmUniboCommon::Engine => "/dm_unibo_common"
+
+  get '/choose_organization', to: "home#choose_organization"
+
+  resources :helps
+  resources :organizations
+
+  scope ":__org__" do
+    resources :permissions
+
+    resources :vendors
+    resources :printer_models do 
+      resources :printers
+    end
+    resources :toner_models do
+      resources :toners
+    end
+
     resources :printers
-    resources :toners
+    resources :toners 
+
+    post 'search', to: 'home#search', as: 'search'
+    get  'report', to: 'home#report', as: 'report'
+
+    resources  :subscriptions
+
+    get '/', to: 'home#index', as: 'current_organization_root'
   end
-
-  resources :admins
-
-  resources :vendors
-  resources :printer_models do 
-    resources :printers
-  end
-  resources :toner_models do
-    resources :toners
-  end
-
-  resources :printers
-  resources :toners 
-
-  post 'search', to: 'home#search', as: 'search'
-  get  'report', to: 'home#report', as: 'report'
-
-  resources  :subscriptions
-
-  root 'home#index'
+  root to: 'home#index'
 end
