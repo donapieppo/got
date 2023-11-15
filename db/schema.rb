f050cea8da4257786e99638356c296650b983ae5
Pietro <donapieppo@yahoo.it>
2023-11-15 13:50:22 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_130512) do
-
-  create_table "organizations", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_130929) do
+  create_table "organizations", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.string "code", limit: 250
     t.string "name"
     t.string "description"
-    t.datetime "updated_at"
-    t.datetime "created_at"
   end
 
-  create_table "permissions", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "permissions", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "user_id", unsigned: true
     t.integer "organization_id", unsigned: true
     t.string "network", limit: 20
     t.integer "authlevel"
-    t.datetime "updated_at"
-    t.datetime "created_at"
     t.index ["organization_id"], name: "fk_organization_permission"
     t.index ["user_id"], name: "fk_user_permission"
   end
 
-  create_table "printer_models", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "printer_models", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "vendor_id", null: false, unsigned: true
     t.string "name"
     t.boolean "laser"
     t.index ["vendor_id"], name: "vendor_id"
   end
 
-  create_table "printer_models_toner_models", id: false, charset: "utf8", force: :cascade do |t|
+  create_table "printer_models_toner_models", id: false, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "printer_model_id", null: false, unsigned: true
     t.integer "toner_model_id", null: false, unsigned: true
     t.index ["printer_model_id"], name: "printer_model_id"
     t.index ["toner_model_id"], name: "toner_model_id"
   end
 
-  create_table "printers", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "printers", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "organization_id", unsigned: true
     t.integer "printer_model_id", unsigned: true
     t.string "name"
@@ -55,14 +50,14 @@ ActiveRecord::Schema.define(version: 2020_05_05_130512) do
     t.index ["printer_model_id"], name: "printer_model_id"
   end
 
-  create_table "toner_models", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "toner_models", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "vendor_id", null: false, unsigned: true
     t.string "name"
     t.boolean "compatible"
     t.index ["vendor_id"], name: "vendor_id"
   end
 
-  create_table "toners", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "toners", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.integer "organization_id", unsigned: true
     t.integer "toner_model_id", unsigned: true
     t.integer "number", default: 0, unsigned: true
@@ -71,16 +66,16 @@ ActiveRecord::Schema.define(version: 2020_05_05_130512) do
     t.index ["toner_model_id"], name: "toner_model_id"
   end
 
-  create_table "users", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8", force: :cascade do |t|
+  create_table "users", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.string "upn"
     t.string "email"
     t.string "name"
     t.string "surname"
-    t.timestamp "updated_at", default: -> { "current_timestamp()" }, null: false
+    t.timestamp "updated_at", default: -> { "current_timestamp() ON UPDATE current_timestamp()" }, null: false
     t.index ["upn"], name: "index_users_on_upn"
   end
 
-  create_table "vendors", id: { type: :integer, unsigned: true }, charset: "utf8", force: :cascade do |t|
+  create_table "vendors", id: { type: :integer, unsigned: true }, charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.string "name"
   end
 
