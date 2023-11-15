2921e010100d38a178beea52d7124cf57fe469f6
Pietro <donapieppo@yahoo.it>
2023-11-15 13:52:05 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,24 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Got
   class Application < Rails::Application
-    config.autoload_paths += %W(#{Rails.root}/app/pdfs)
-    config.time_zone = 'Rome'
+    config.autoload_paths << "#{Rails.root}/app/pdfs"
+    config.time_zone = "Rome"
 
     config.load_defaults 7.0
 
     config.i18n.default_locale = :it
 
-    config.authlevels = { read: 1, manage: 2 }
+    config.authlevels = {read: 1, manage: 2}
 
-    config.action_mailer.default_url_options = { protocol: 'https' }
+    config.action_mailer.default_url_options = {protocol: "https"}
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
+
   end
 end
