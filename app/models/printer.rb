e90c49e1646b1c73baa403ae4d8afcd761ea75ca
Pietro <donapieppo@yahoo.it>
2023-11-17 14:46:52 +0100

    validates

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -2,9 +2,8 @@ class Printer < ApplicationRecord
   belongs_to :organization
   belongs_to :printer_model
 
-  validates :name, presence: {}
-  validates :name, uniqueness: { scope: [:organization_id], message: "La stampante è già stata registrata nella struttura." }
-
+  validates :name, presence: {},
+    uniqueness: {scope: [:organization_id], message: "La stampante è già stata registrata nella struttura."}
   validates :organization, presence: {}
   validates :printer_model, presence: {}
 
