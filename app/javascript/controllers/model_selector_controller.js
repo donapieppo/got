c12d59e75834d362d57a2240276613ab5c739af1
Pietro <donapieppo@yahoo.it>
2023-11-23 09:52:42 +0100

    BIG refactor in model_selector_controller

--- app/javascript/controllers/model_selector_controller.js
+++ app/javascript/controllers/model_selector_controller.js
@@ -1,31 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
+// check the vendor and update the available models
 export default class extends Controller {
+  // { "1": [173, 172, 136], "3": [1, 2, 22]
   static values = { vendorToModels: Object }
+  static targets = [ "vendor", "model" ]
 
   connect() {
     this.show_vendor_printers();
-    // console.log(this.vendorToModelsValue);
+    console.log(this.vendorToModelsValue);
+    console.log(this.vendorTarget);
+    console.log(this.modelTarget);
   }
 
   change(event) {
-    console.log("changed printer_vendor");
-    document.querySelector("#printer_printer_model_id option:checked").selected = false;
+    console.log("changed vendor");
+    this.modelTarget.querySelector("option:checked").selected = false;
     this.show_vendor_printers();
   }
 
   hide_all() {
-    document.querySelectorAll("#printer_printer_model_id option").forEach ((x) => (x.style.display = "none"));
+    this.modelTarget.querySelectorAll("option").forEach ((x) => (x.style.display = "none"));
   }
 
   show_vendor_printers() {
-    var vendor = document.querySelector("#printer_vendor option:checked");
-    console.log(`Changed -> ${vendor.value}`);
+    var vendor = this.vendorTarget.querySelector("option:checked");
+    console.log(`Vendor checked: ${vendor.value}`);
     var models = this.vendorToModelsValue[parseInt(vendor.value)];
 
     this.hide_all();
     models.forEach((i) => {
-      document.querySelectorAll(`#printer_printer_model_id option[value="${i}"]`).forEach ((x) => (x.style.display = "block"))
+      console.log(i);
+      this.modelTarget.querySelectorAll(`option[value="${i}"]`).forEach ((x) => (x.style.display = "block"))
     });
   }
 }
